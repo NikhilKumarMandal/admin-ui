b57fcb380918e62f7138df241ed5a8770023c7e0
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-09-02 16:52:03 +0530

    add protected routes

--- src/Layout/NoAuth.tsx
+++ src/Layout/NoAuth.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuthStore } from "../store/store"
 
 
 function NoAuth() {
+  const { user } = useAuthStore()
+    if (user !== null) {
+        return <Navigate to="/" replace={ true } />
+    }
   return (
     <Outlet/>
   )
