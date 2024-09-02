1ea7422855eae2ba702e32c4a3eb2d824f7558f6
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-09-02 11:57:23 +0530

    add self endpoint

--- src/http/api.ts
+++ src/http/api.ts
@@ -4,4 +4,6 @@ import { api } from './client';
 
 // Auth Service
 
-export const login = (credentials: Credentials) => api.post("/api/v1/auth/login",credentials)
+export const login = (credentials: Credentials) => api.post("/api/v1/auth/login", credentials)
+
+export const self = () => api.get("/api/v1/auth/self")
