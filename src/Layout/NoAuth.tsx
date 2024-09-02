8bb1229230eacd7cb11f700ba6f212aacedce65d
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-09-02 16:39:29 +0530

    add layouts

---
+++ src/Layout/NoAuth.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom"
+
+
+function NoAuth() {
+  return (
+    <Outlet/>
+  )
+}
+
+export default NoAuth
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/Login/loginPage";
+import Dashboard from "./Layout/Dashboard";
+import NoAuth from "./Layout/NoAuth";
 
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <HomePage/>
+        element: <Dashboard />,
+        children: [
+            {
+                path: '',
+                element: <HomePage/>
+            }
+        ]
     },
     {
-        path: "/auth/login",
-        element: <LoginPage/>
+        path: "/auth",
+        element: <NoAuth />,
+        children: [
+            {
+                path: "login",
+                element: <LoginPage/>
+            }
+        ]
     }
+
 ])
